e9c6f9fe9889d45ff3d3a50d8342d513917ae912
maheshglm1 <maheshglm1@gmail.com>
2018-09-18 18:30:48 +0800

    Add tests for compareSvc methods

--- src/test/java/com/gojek/program/svc/CompareSvcRunIT.java
+++ src/test/java/com/gojek/program/svc/CompareSvcRunIT.java
@@ -1,5 +1,53 @@
+package com.gojek.program.svc;
+
+import com.gojek.program.SpringTestConfig;
+import com.gojek.program.exceptions.Exception;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {SpringTestConfig.class})
 public class CompareSvcRunIT {
 
+    private static final File FILE1 = new File("target/test-classes/gojek/file1.txt");
+    private static final File FILE2 = new File("target/test-classes/gojek/file2.txt");
+
+    @Autowired
+    private CompareSvc compareSvc;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testCompareApiResponses_successful() {
+        try {
+            compareSvc.compareApiResponses(FILE1, FILE2);
+        }catch (Exception e){
+            Assert.assertTrue(false);
+        }
+    }
+
+    @Test
+    public void testCompareApiResponses_file1NotFound(){
+        thrown.expect(Exception.class);
+        thrown.expectMessage("File [C:\\Users\\mahesh\\compare-response\\noFile.txt] is not available");
+        compareSvc.compareApiResponses(new File("noFile.txt"),FILE2);
+    }
+
+    @Test
+    public void testCompareApiResponses_file2NotFound(){
+        thrown.expect(Exception.class);
+        thrown.expectMessage("File [C:\\Users\\mahesh\\compare-response\\noFile.txt] is not available");
+        compareSvc.compareApiResponses(FILE1,new File("noFile.txt"));
+    }
 }
--- src/test/java/com/gojek/program/svc/CompareSvcTest.java
+++ src/test/java/com/gojek/program/svc/CompareSvcTest.java
@@ -1,4 +1,97 @@
 package com.gojek.program.svc;
 
+import com.gojek.program.exceptions.Exception;
+import com.gojek.program.mdl.ResponseSpec;
+import com.gojek.program.utils.JsonUtil;
+import com.gojek.program.utils.XmlUtil;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static com.gojek.program.svc.CompareSvc.APPLICATION_JSON;
+import static com.gojek.program.svc.CompareSvc.APPLICATION_XML;
+import static com.gojek.program.svc.CompareSvc.UNSUPPORTED_CONTENT_TYPE_FOR_COMPARISION;
+
 public class CompareSvcTest {
+
+    @InjectMocks
+    private CompareSvc compareSvc;
+
+    @Mock
+    private JsonUtil jsonUtil;
+
+    @Mock
+    private XmlUtil xmlUtil;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testCompare_Xml() {
+        String resp = "<a><b>test</b></a>";
+
+        ResponseSpec spec1 = new ResponseSpec();
+        ResponseSpec spec2 = new ResponseSpec();
+
+        spec1.setContentType(APPLICATION_XML);
+        spec1.setResponse(resp);
+        spec2.setContentType(APPLICATION_XML);
+        spec2.setResponse(resp);
+
+        Mockito.when(xmlUtil.compareXml(resp, resp)).thenReturn(true);
+
+        Assert.assertTrue(compareSvc.compare(spec1, spec2));
+
+        Mockito.verify(xmlUtil, Mockito.timeout(1)).compareXml(resp, resp);
+    }
+
+    @Test
+    public void testCompare_json() {
+        String resp = "{\"key\":\"value\"}";
+
+        ResponseSpec spec1 = new ResponseSpec();
+        ResponseSpec spec2 = new ResponseSpec();
+
+        spec1.setContentType(APPLICATION_JSON);
+        spec1.setResponse(resp);
+        spec2.setContentType(APPLICATION_JSON);
+        spec2.setResponse(resp);
+
+        Mockito.when(jsonUtil.compareJson(resp, resp)).thenReturn(true);
+
+        Assert.assertTrue(compareSvc.compare(spec1, spec2));
+
+        Mockito.verify(jsonUtil, Mockito.timeout(1)).compareJson(resp, resp);
+    }
+
+    @Test
+    public void testCompare_invalidContentType() {
+        thrown.expect(Exception.class);
+        thrown.expectMessage(UNSUPPORTED_CONTENT_TYPE_FOR_COMPARISION);
+
+        String resp = "{\"key\":\"value\"}";
+
+        ResponseSpec spec1 = new ResponseSpec();
+        ResponseSpec spec2 = new ResponseSpec();
+
+        spec1.setContentType("application/text");
+        spec1.setResponse(resp);
+        spec2.setContentType("application/text");
+        spec2.setResponse(resp);
+
+        Assert.assertTrue(compareSvc.compare(spec1, spec2));
+
+        Mockito.verify(jsonUtil, Mockito.timeout(0)).compareJson(resp, resp);
+    }
+
+
 }
+
