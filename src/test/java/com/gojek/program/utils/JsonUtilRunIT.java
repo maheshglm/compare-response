f014ee52bb90cd1fe3bdcf39aeb0fddf4e84b843
maheshglm1 <maheshglm1@gmail.com>
2018-09-18 17:14:03 +0800

    Add new tests for jsoncomparision

--- src/test/java/com/gojek/program/utils/JsonUtilRunIT.java
+++ src/test/java/com/gojek/program/utils/JsonUtilRunIT.java
@@ -1,4 +1,54 @@
 package com.gojek.program.utils;
 
+import com.gojek.program.SpringTestConfig;
+import com.gojek.program.exceptions.Exception;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.gojek.program.utils.JsonUtil.IO_ERROR_WHILE_PARSING_JSON_STRING;
+import static com.gojek.program.utils.XmlUtil.IO_ERROR_WHILE_PARSING_XML_STRING;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {SpringTestConfig.class})
 public class JsonUtilRunIT {
+
+    private static final String JSON1 = "target/test-classes/gojek/json/file1.json";
+    private static final String JSON2 = "target/test-classes/gojek/json/file2.json";
+
+    @Autowired
+    private JsonUtil jsonUtil;
+
+    @Autowired
+    private FileDirUtil fileDirUtil;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testJsonCompare_successfulComparison() {
+        final String json1Data = fileDirUtil.readFileToString(JSON1);
+        final String json2Data = fileDirUtil.readFileToString(JSON1);
+        Assert.assertTrue(jsonUtil.compareJson(json1Data, json2Data));
+    }
+
+    @Test
+    public void testJsonCompare_unSuccessfulComparison() {
+        final String json1Data = fileDirUtil.readFileToString(JSON1);
+        final String json2Data = fileDirUtil.readFileToString(JSON2);
+        Assert.assertFalse(jsonUtil.compareJson(json1Data, json2Data));
+    }
+
+    @Test
+    public void testJsonCompare_exception(){
+        thrown.expect(Exception.class);
+        thrown.expectMessage(IO_ERROR_WHILE_PARSING_JSON_STRING);
+        jsonUtil.compareJson("{\"name\": \"John\"","{\"name\": \"John\"}");
+    }
+
 }
